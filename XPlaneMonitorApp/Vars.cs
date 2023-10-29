28dfb69d355d4ffef5e95227905cf1f0323b2528
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2023-10-29 10:37:43 -0300

    version 1.2

--- XPlaneMonitorApp/Vars.cs
+++ XPlaneMonitorApp/Vars.cs
@@ -4,7 +4,7 @@ namespace XPlaneMonitorApp
 {
     public class Vars
     {
-        public const string VERSION = "1.1";
+        public const string VERSION = "1.2";
 
         public static ConfigData Cfg;
     }
